0f51c0681ba6a36a21f8e2aea540a9fcb538523f
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-04 19:33:59 -0300

    :sparkles: feat: Create HomeHeader component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,26 +1,12 @@
-import { Text, View, Button } from 'react-native'
-import { router } from 'expo-router'
-import { fontFamily } from '@/theme/fontFamily'
+import { HomeHeader } from '@/components/HomeHeader'
+import { View } from 'react-native'
 
 export default function Index() {
 
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ fontFamily: fontFamily.bold, fontSize: 34 }}>Expo Router</Text>
-
-      <Button
-        title='Nova Meta'
-        onPress={() => router.navigate("/target")}
-      />
-      <Button
-        title='Transação'
-        onPress={() => router.navigate("/transaction/123")}
-      />
-      <Button
-        title='Progresso'
-        onPress={() => router.navigate("/in-progress/123")}
-      />
+    <View style={{ flex: 1 }}>
+      <HomeHeader />
     </View>
   )
 }
