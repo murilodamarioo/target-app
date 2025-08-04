11a9538beecf2dc02abf4911f88f95974f51e344
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-04 19:48:01 -0300

    :recycle: refactor: Unifies imports

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,3 +1,4 @@
+import { View, Text } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 
 import { styles } from './styles'
@@ -5,9 +6,10 @@ import { colors } from '@/theme/colors'
 
 export function HomeHeader() {
   return (
-    <LinearGradient
-      colors={[colors.blue[500], colors.blue[800]]}
-      style={styles.container}
-    ></LinearGradient>
+    <LinearGradient colors={[colors.blue[500], colors.blue[800]]} style={styles.container}>
+      <View>
+        <Text style={styles.label}>Total que você possui</Text>
+      </View>
+    </LinearGradient>
   )
 }
--- src/components/HomeHeader/styles.ts
+++ src/components/HomeHeader/styles.ts
@@ -1,8 +1,18 @@
+import { colors, fontFamily } from '@/theme'
 import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
   container: {
     width: '100%',
-    height: 324
+    height: 324,
+    paddingHorizontal: 24,
+    paddingBottom: 18,
+    justifyContent: 'flex-end',
+    gap: 24
+  },
+  label: {
+    fontSize: 12,
+    color: colors.white,
+    fontFamily: fontFamily.regular
   }
 })
