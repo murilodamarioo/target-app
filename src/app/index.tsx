ee31d05cec7fe024b20ce49c104af4fdd7507548
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-04 22:20:56 -0300

    :recycle: refactor: Reusing types

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,16 @@
 import { HomeHeader } from '@/components/HomeHeader'
 import { View } from 'react-native'
 
+const summary = {
+  total: 'R$ 2.6880,00',
+  input: { label: 'Entradas', value: 'R$ 6.184,00' },
+  output: { label: 'Saídas', value: '-R$ 883,65' }
+}
+
 export default function Index() {
-
-
   return (
     <View style={{ flex: 1 }}>
-      <HomeHeader data={{ total: 'R$ 2.6880,00' }} />
+      <HomeHeader data={summary} />
     </View>
   )
 }
