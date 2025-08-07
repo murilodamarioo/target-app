c9570ce9e057e5b2853d86e0e361bc2d245401dd
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-07 18:26:21 -0300

    :sparkles: feat: Create Progress component and implements on Progress screen

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,17 +1,29 @@
-import { View, Text, Button } from 'react-native'
-import { router, useLocalSearchParams } from 'expo-router'
+import { View } from 'react-native'
+import { useLocalSearchParams } from 'expo-router'
+
+import { PageHeader } from '@/components/PageHeader'
+import { Progress } from '@/components/Progress'
+
+const details = {
+  current: 'R$ 1.000,00',
+  target: 'R$ 2.000,00',
+  percentage: 50
+}
 
 export default function InProgress() {
   const params = useLocalSearchParams<{ id: string }>()
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>InProgress: {params.id}</Text>
-
-      <Button
-        title='Voltar'
-        onPress={() => router.back()}
+    <View style={{ flex: 1, padding: 24, gap: 32 }}>
+      <PageHeader
+        title='Monitor LG'
+        rightButton={{
+          icon: 'edit',
+          onPress: () => { },
+        }}
       />
+
+      <Progress data={details} />
     </View>
   )
 }
