b13ad97463c69a9265363026697e2cccf70f4237
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-09 20:18:14 -0300

    :sparkles: feat: Create Transactions table migrate

--- src/database/migrate.ts
+++ src/database/migrate.ts
@@ -11,6 +11,19 @@ export async function migrate(database: SQLiteDatabase) {
         created_at timestamp NOT NULL DEFAULT current_timestamp,
         updated_at timestamp NOT NULL DEFAULT current_timestamp
       );
-      
+
+
+      CREATE TABLE IF NOT EXISTS transactions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        target_id INTEGER NOT NULL,
+        amount FLOAT NOT NULL,
+        observation TEXT NULL,
+        created_at timestamp NOT NULL DEFAULT current_timestamp,
+        updated_at timestamp NOT NULL DEFAULT current_timestamp
+
+        CONSTRAINT fk_target_transactions 
+        FOREIGN KEY (target_id) REFERENCES targets (id)
+        ON DELETE CASCATE;
+      );
     `)
 }
