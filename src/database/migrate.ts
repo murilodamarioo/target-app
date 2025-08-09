64dac471df7df1b1a7491a61b47f252240843b95
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-09 20:37:30 -0300

    :bug: fix: Adjust on SQL migrate

--- src/database/migrate.ts
+++ src/database/migrate.ts
@@ -1,29 +1,30 @@
 import { type SQLiteDatabase } from 'expo-sqlite'
 
 export async function migrate(database: SQLiteDatabase) {
-  await database.execAsync(`
-      PRAGMA foreign_keys = ON;
+  return database.execAsync(`
+      PRAGMA foreign_key = ON;
 
       CREATE TABLE IF NOT EXISTS targets (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         amount FLOAT NOT NULL,
+
         created_at timestamp NOT NULL DEFAULT current_timestamp,
         updated_at timestamp NOT NULL DEFAULT current_timestamp
       );
-
 
       CREATE TABLE IF NOT EXISTS transactions (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         target_id INTEGER NOT NULL,
         amount FLOAT NOT NULL,
         observation TEXT NULL,
-        created_at timestamp NOT NULL DEFAULT current_timestamp,
-        updated_at timestamp NOT NULL DEFAULT current_timestamp
 
-        CONSTRAINT fk_target_transactions 
-        FOREIGN KEY (target_id) REFERENCES targets (id)
-        ON DELETE CASCATE;
+        created_at timestamp NOT NULL DEFAULT current_timestamp,
+        updated_at timestamp NOT NULL DEFAULT current_timestamp,
+
+        CONSTRAINT fk_transactions_target
+        FOREIGN KEY (target_id) REFERENCES targets(id)
+        ON DELETE CASCADE
       );
     `)
 }
