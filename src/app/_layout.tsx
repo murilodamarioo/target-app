0890922e1e513b1386d16993c9806e0c83e19118
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-09 20:28:30 -0300

    :sparkles: feat: Create DB connection

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import { Stack } from 'expo-router'
 import {
   useFonts,
@@ -5,6 +6,10 @@ import {
   Inter_500Medium,
   Inter_700Bold
 } from '@expo-google-fonts/inter'
+
+import { SQLiteProvider } from 'expo-sqlite'
+
+import { migrate } from '@/database/migrate'
 
 import { colors } from '@/theme/colors'
 
@@ -22,11 +27,15 @@ export default function Layout() {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: colors.white },
-      }}
-    />
+    <Suspense fallback={<Loading />}>
+      <SQLiteProvider databaseName='target.db' onInit={migrate} useSuspense>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: colors.white },
+          }}
+        />
+      </SQLiteProvider>
+    </Suspense>
   )
 }
