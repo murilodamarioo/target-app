c5753dab1b35df8bea3e498879742922acc210c7
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-14 16:51:28 -0300

    :sparkles: feat: Implements show target on Target screen and render values

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,19 +1,18 @@
-import { View } from 'react-native'
-import { useLocalSearchParams, router } from 'expo-router'
+import { useCallback, useState } from 'react'
+import { Alert, View } from 'react-native'
+import { useLocalSearchParams, router, useFocusEffect } from 'expo-router'
 
 import { PageHeader } from '@/components/PageHeader'
 import { Progress } from '@/components/Progress'
 import { List } from '@/components/List'
 import { Button } from '@/components/Button'
 import { Transaction, TransactionProps } from '@/components/Transaction'
+import { Loading } from '@/components/Loading'
 
 import { TransactionTypes } from '@/utils/TransactionTypes'
 
-const details = {
-  current: 'R$ 1.000,00',
-  target: 'R$ 2.000,00',
-  percentage: 50
-}
+import { useTargetDatabase } from '@/database/useTargetDatabase'
+import { numberToCurrency } from '@/utils/number-to-currency'
 
 const transactions: TransactionProps[] = [
   {
@@ -32,12 +31,54 @@ const transactions: TransactionProps[] = [
 ]
 
 export default function InProgress() {
+  const [isFetching, setIsFetching] = useState(true)
+  const [details, setDetails] = useState({
+    name: '',
+    current: 'R$ 0,00',
+    target: 'R$ 0,00',
+    percentage: 0
+  })
+
   const params = useLocalSearchParams<{ id: string }>()
+
+  const targetDatabase = useTargetDatabase()
+
+  async function fetchDetails() {
+    try {
+      const response = await targetDatabase.show(Number(params.id))
+
+      setDetails({
+        name: response.name,
+        current: numberToCurrency(response.current),
+        target: numberToCurrency(response.amount),
+        percentage: response.percentage
+      })
+    } catch (error) {
+      Alert.alert('Erro', 'Nao foi possivel carregar os detalhes da meta.')
+      console.log(error)
+    }
+  }
+
+  async function fetchData() {
+    const fetchDetailsPromise = fetchDetails()
+
+    await Promise.all([fetchDetailsPromise])
+    setIsFetching(false)
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchData()
+    }, []),
+  )
+
+  if (isFetching)
+    return <Loading />
 
   return (
     <View style={{ flex: 1, padding: 24, gap: 32 }}>
       <PageHeader
-        title='Monitor LG'
+        title={details.name}
         rightButton={{
           icon: 'edit',
           onPress: () => { },
